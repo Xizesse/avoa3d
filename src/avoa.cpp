583a2c449f6bdd18cad61a3e01447afc2d424b80
xizesse <kikopiressilva@gmail.com>
2024-09-05 15:23:36 +0100

    Added test node : Goal with proportional velocity

--- src/avoa.cpp
+++ src/avoa.cpp
@@ -14,27 +14,35 @@ class avoa : public rclcpp::Node
         depth_m_(30.0),
         resolution_m(0.2f)
         height_cells_(static_cast<int>(height_m / resolution_m)),
-        width_cells_(static_cast<int>(width_m_ / resolution_)),
-        depth_cells_(static_cast<int>(depth_m_ / resolution_)),
-        space_(height_cells_, std::vector<std::vector<int>>(width_cells_, std::vector<int>(depth_cells_, 0)))
-
+        width_cells_(static_cast<int>(width_m_ / resolution_m)),
+        depth_cells_(static_cast<int>(depth_m_ / resolution_m)),
         {
             timer_ = this->create_wall_timer(1000ms, std::bind(&avoa::timer_callback, this));
         }
     private:
+
         void timer_callback()
         {
             RCLCPP_INFO(this->get_logger(), "Loop");
+            //! Subscrever a posição e a velocidade do agente
+            //! Subscrever a posição e a velocidade do obstaculo 
+
+
+            //! Criar o espaço com as coordenadas do agente + offset do espaço
+            
+            //!PSO
+            //! A cada elemento
+                //! Aceleração máxima
+                //! Restrições de 
+                //! Verificar geometricamente se é protective zone
+                //! Marcar se for protective zone
+
+            //! 
+            //! 
+
+
         }
-    float height_m_;
-    float width_m_;
-    float depth_m_;
-    float resolution_;
-    int height_cells_;
-    int width_cells_;
-    int depth_cells_;
-    std::vector<std::vector<std::vector<int>>> space_;
-    rclcpp::TimerBase::SharedPtr timer_;
+    
 };
 
 int main(int argc, char * argv[])
